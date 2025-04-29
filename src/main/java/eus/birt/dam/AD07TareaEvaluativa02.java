23894acb28a33a0b91bf586f28864d94e6f9b050
Luken <lcamarero@birt.eus>
2025-04-29 20:05:37 +0200

    Commit inicial: proyecto de tarea de aprendizaje 03

---
+++ src/main/java/eus/birt/dam/AD07TareaEvaluativa02.java
@@ -0,0 +1,15 @@
+
+
+package eus.birt.dam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AD07TareaEvaluativa02 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AD07TareaEvaluativa02.class, args);
+	}
+
+}
